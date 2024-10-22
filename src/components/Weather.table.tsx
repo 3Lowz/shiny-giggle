2858ba37cdeafcec01c2c39337f84eb734485518
3lowz <hdaneluz@hotmail.com>
2024-10-22 18:32:51 +0200

    added charts with d3js

--- src/components/Weather.table.tsx
+++ src/components/Weather.table.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { WeatherData } from '../services/Weather'
 import WeatherList from './Weather.list'
+import ChartRef from './Chart'
 
 export type DataTable = {
     data: WeatherData
@@ -9,15 +10,21 @@ export type DataTable = {
 const WeatherTable = (props: DataTable): React.ReactElement => {
 
     // const service = useContext(WeatherContext)
-    let lists = []
+    let lists: React.ReactElement[] = []
+    let charts: React.ReactElement[] = []
     for (let key in props.data) {
         // @ts-ignore
         lists.push(<WeatherList name={key} key={key} unitMeasure={props.data[key].unitMeasure} values={props.data[key].values} />)
+        // @ts-ignore
+        charts.push(<ChartRef name={key} key={key} unitMeasure={props.data[key].unitMeasure} values={props.data[key].values} />)
     }
 
     return (<>
         {lists.map((list) => {
             return list
+        })}
+        {charts.map((chart) => {
+            return chart
         })}
     </>)
 }
