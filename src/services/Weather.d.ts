3a70ba5507ed1b8fea7ae73644891d1ab0778221
3lowz <hdaneluz@hotmail.com>
2024-10-22 19:17:48 +0200

    fixed service and repository types

--- src/services/Weather.d.ts
+++ src/services/Weather.d.ts
@@ -4,7 +4,7 @@ export type WeatherAtom = [string, number]
 export type WeatherValues = 'temperature_2m' | 'relative_humidity_2m' | 'precipitation' | 'wind_speed_10m'
 
 export type WeatherData = {
-    [key: WeatherValues]: {
+    [K in keyof WeatherValues]: {
         unitMeasure: UnitMeasure,
         values: WeatherAtom[]
     }
@@ -30,7 +30,7 @@ export type WeatherInfo = {
         relative_humidity_2m: HumidityMetrics,
         precipitation: PrecipitationMetrics,
         wind_speed_10m: WindMetrics
-        [key: WeatherValues]: UnitMeasure
+        // [key: WeatherValues]: UnitMeasure   // generic
     },
     hourly: {
         time: string[],
@@ -43,16 +43,31 @@ export type Coordinates = [number, number]
 export type ResultType<E = Error, T> = [Error?, T?]
 
 interface Repository<T> {
+    private service: T
+    constructor(service: T)
     getById(K: any): T
     getList(): T[]
 }
 
-interface WeatherRepository extends Repository<WeatherInfo> {
-    // List<Article> read();
-    async getByCity(city: string): WeatherInfo
+interface WeatherRepository extends Repository<WeatherService> {
+    constructor(service: WeatherService)
+    async getByCity(city: string): Promise<ResultType<Error, WeatherData>>
 }
 
 interface WeatherService {
+    private BASEURL: string
+    private filters: Filter
+    private cities: City[]
+    private city: City
     async getByCity(city: string): Promise<ResultType<Error, WeatherInfo>>
     async getByCoords(lat: number, lng: number): Promise<ResultType<Error, WeatherInfo>>
+    async getByCity(cityName: string): Promise<ResultType<Error, WeatherData>>
+    getByCoords(lat: number, lng: number): Promise<ResultType<Error, WeatherInfo>>
+    private _buildUrl(position: Coordinates, activeFilters: string[])
+    private _transformData(data: WeatherInfo): WeatherData
+    getFilters()
+    setFilter(filters: Filter)
+    getCities()
+    setCity(city: City)
+    getCity()
 }
